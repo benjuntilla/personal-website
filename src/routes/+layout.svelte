c6436334fac5c3fd903219fe9758e67a8539cae7
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-09-15 21:27:19 -0700

    change font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
     import "../app.css";
+    import "@fontsource/vt323";
 </script>
+
+<style lang="postcss">
+    :global(body) {
+        font-family: "VT323";
+    }
+</style>
 
 <slot />
