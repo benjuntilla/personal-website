620c3b0580365bfe7d7c27cfeda1904285ecf7c1
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-12-19 05:09:26 -0700

    add cool bar of links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import App from "$lib/components/App.svelte";
+  import { LinkedinSolid, GithubSolid, InfoCircleOutline } from "flowbite-svelte-icons";
 </script>
 
 <div class="w-full h-full">
@@ -7,7 +8,7 @@
     <App />
   </div>
 
-  <div class="relative pointer-events-none w-full mx-[2%]" style:container-type="inline-size">
+  <div class="relative h-max mx-[2%] flex flex-col" style:container-type="inline-size">
     <h1 class="text-[18cqw] leading-[18cqw] lg:text-[10cqw] lg:leading-[10cqw] font-pixel">
       BEN JUNTILLA
     </h1>
@@ -17,5 +18,30 @@
         >software developer</span
       >
     </h1>
+
+    <div class="justify-center mt-[5%] lg:mt-[15%] logo-cloud grid-cols-1 lg:!grid-cols-3 gap-1">
+      <a class="logo-item" href="/about">
+        <span><InfoCircleOutline /></span>
+        <span>About</span>
+      </a>
+      <a
+        class="logo-item"
+        href="https://github.com/benjuntilla"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <span><GithubSolid /></span>
+        <span>GitHub</span>
+      </a>
+      <a
+        class="logo-item"
+        href="https://linkedin.com/in/benjuntilla"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <span><LinkedinSolid /></span>
+        <span>LinkedIn</span>
+      </a>
+    </div>
   </div>
 </div>
