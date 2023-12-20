2a83a5b135f0ea261102849753962f55c24212de
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-12-19 20:52:33 -0700

    center about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,11 @@
-<article class="prose lg:prose-xl dark:prose-invert">
-  <h1>about</h1>
-  <p>
-    Hey! 👋 I'm a student software developer studying computer science at Arizona State University.
-    Check out my <a href="/portfolio">portfolio</a> to get a taste of what I do! If you want to talk
-    some more, refer to my social profiles linked
-    <a href="/">here</a>.
-  </p>
-</article>
+<div class="w-full grid place-content-center">
+  <article class="prose lg:prose-xl dark:prose-invert">
+    <h1>about</h1>
+    <p>
+      Hey! 👋 I'm a student software developer studying computer science at Arizona State
+      University. Check out my <a href="/portfolio">portfolio</a> to get a taste of what I do! If
+      you want to talk some more, refer to my social profiles linked
+      <a href="/">here</a>.
+    </p>
+  </article>
+</div>
