0bd62604bc3e9759a2d5ddb2bad8fd91ccb87aaf
Ben Juntilla <ben@benjuntilla.com>
2024-01-17 14:17:03 -0700

    update bio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,24 +6,25 @@
   <article class="prose dark:prose-invert max-w-[650px] prose-code:before:hidden prose-code:after:hidden">
     <h1 class="h1">about</h1>
     <p>
-      Hey! 👋 I'm Ben, nice to meet you! I'm an honors undergraduate
-      studying computer science in the beautiful, sunny campus of
-      Arizona State University. I've been building software for
-      several years now, and my experience includes full-stack and
+      // Hey! 👋 I'm Ben, nice to meet you! I'm an honors
+      undergraduate studying computer science in the beautiful, sunny
+      campus of Arizona State University. I've been building software
+      for several years now, and my experience includes full-stack and
       low-level development, in such languages as <code>Python</code>,
       <code>Javascript</code>, <code>Typescript</code>
-      and <code>C</code>. Check out my <a class="anchor"
+      and <code>C</code>. Check out my website's <a class="anchor"
       href="/portfolio">portfolio</a> to get a taste of what I do!
     </p>
     <p>
-      Much of my previous work laid in web development, but I'm
+      // Much of my previous work laid in web development, but I'm
       looking to sharpen my skills in systems programming, data
       science, and machine learning.
     </p>
     <p>
-      I'm always excited for new opportunities to learn, grow, and
-      best of all, build. Refer to my social profiles
-      linked <a class="anchor" href="/">here</a>, and let's connect!
+      // I'm always excited for new opportunities to learn, grow, and
+      best of all, build. Refer to my social profiles linked on my
+      website's <a class="anchor" href="/">home page</a>, and let's
+      connect!
     </p>
   </article>
 </div>
